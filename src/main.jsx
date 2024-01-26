ef54c7f9ef67c6fba0f03d608f175ce7a5687df1
Rob Hess <hessro@oregonstate.edu>
2024-01-26 13:30:47 -0800

    Another minor update.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { Home } from './Routes'
-
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <h1>Hello, world!</h1>
   </React.StrictMode>,
 )
