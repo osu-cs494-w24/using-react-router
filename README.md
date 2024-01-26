99c67983acd3836968c914960c3c72aca6dfc629
Rob Hess <hessro@oregonstate.edu>
2024-01-26 12:07:27 -0800

    Add initial code.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Client-Side Rouing with React Router
+
+We'll use this code as a starting point for exploring how to implement client-side routing with [React Router](https://reactrouter.com/).
+
+The first time you run this app, make sure to run `npm install` first, to install needed dependencies.  Then, to run the app and see it in your browser, you can run
+```
+npm run dev
+```
+This will run the app using Vite's development server.  You should be able to visit the running app by viewing [http://localhost:5173](http://localhost:5173) to view the app in your browser.  The development server is set up to use "Hot Module Replacement" (HMR), so the app will automatically reload in your browser if you make edits to the code.
---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import './App.css'
+
+const menu = {
+    tacos: {
+        name: "Tacos",
+        image: "https://media.giphy.com/media/KfxPgR9Xb6lRvlFa8x/giphy.gif",
+        description: "Shell + fillings",
+        price: 5.95
+    },
+    pizza: {
+        name: "Pizza",
+        image: "https://media.giphy.com/media/VCDSo9xqCJOjC/giphy.gif",
+        description: "Crust, sauce, cheese",
+        price: 19.95
+    },
+    sushi: {
+        name: "Sushi",
+        image: "https://media1.tenor.com/images/a7087e13ce68524779c9b6946818986b/tenor.gif",
+        description: "Raw fish + rice",
+        price: 10.95
+    }
+}
+
+function Home() {
+    return <h1>Home</h1>
+}
+
+function About() {
+    return <h1>About</h1>
+}
+
+function People() {
+    return <h1>People</h1>
+}
+
+function Menu() {
+    return <h1>Menu</h1>
+}
+
+function App() {
+    return (
+        <Home />
+    )
+}
+
+export default App
